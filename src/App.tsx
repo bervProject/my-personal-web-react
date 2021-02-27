fe81d67daf9407b8008c33f0ed40e32ec4a96ee5
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-02-27 10:47:39 +0700

    Init Projects

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+//import './App.css';
+import { NavBasicExample as Nav } from './Nav';
+
+function App() {
+  return (
+    <Nav> </Nav>
+  );
+}
+
+export default App;
